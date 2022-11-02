6d7306b5974c6e4e5fa5454e4fc7578baf7dc38b
Bayworld <abayomikiel@gmail.com>
2022-11-02 05:09:29 +0100

    feat: bio to component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "primeicons/primeicons.css";                               //icons
 import { Avatar } from 'primereact/avatar';
 import { Badge } from 'primereact/badge';
 import 'primeflex/primeflex.css';
+import { Button } from 'primereact/button';
  
 
 function App() {
@@ -17,6 +18,19 @@ function App() {
     </div>
     <div className="p-d-flex p-jc-center">
     <p>Founder of Bayoweb</p>
+    </div>
+
+    <div className="p-d-flex p-jc-center">
+    <div className="p-d-flex p-flex-column">
+         <Button className="p-p-3">
+                <i className="pi pi-youtube px-2"></i>
+                <span className="px-3">Youtube</span>
+         </Button>
+         <Button className="p-p-3">
+                <i className="pi pi-twitter px-2"></i>
+                <span className="px-3">Twitter</span>
+         </Button>
+         </div>
     </div>
     </div>
   );
