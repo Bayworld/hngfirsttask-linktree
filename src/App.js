3690ee0fe84bf54ac775c2b50eac1115327bcb33
Bayworld <abayomikiel@gmail.com>
2022-11-02 04:13:57 +0100

    feat: removed unused code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                               //icons
+
+import { Avatar } from 'primereact/avatar';
+import { Badge } from 'primereact/badge';
+ 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Avatar image="profile.jpg" size="xlarge">
+      <Badge value="4" severity="danger" />
+      </Avatar>
+ 
     </div>
   );
 }
